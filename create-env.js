33d40e6de15f026288a508602c8e3bec2182f88d
J <ice.zjchen@gmail.com>
2023-08-08 17:28:30 +0800

    Create create-env.js

---
+++ create-env.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const envList = [
+  'OPENAI_API_KEY',
+  'OPENAI_API_BASE_URL',
+  'HEAD_SCRIPTS',
+  'SECRET_KEY',
+  'SITE_PASSWORD',
+  'OPENAI_API_MODEL'
+];
+
+fs.writeFileSync('env', envList.map(e => `${e}=${process.env[e] || ''}`).join('\n'));
